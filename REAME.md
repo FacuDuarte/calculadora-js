2ebc6a8f9d914a3d3df09ee63a04020aad30feb6
Facundo <cailloureloaded@outlook.com>
2022-06-28 23:05:08 -0300

    Se agrega javascript y REAME.md

---
+++ REAME.md
@@ -0,0 +1,20 @@
+# Caluladora 🖩
+
+👨‍💻 Calculadora responsiva hecha con las siguententes tecnologías:
+
+- Javascript
+- Css
+- HTML
+
+---
+
+Utiliza la fuente [Roboto](https://fonts.google.com/specimen/Roboto?query=roboto), que pueden ver haciendo click en el enlace.
+
+En javascript, hace uso de los siguentes recursos:
+
+- Clases
+- Constructores 
+- Template stings
+- Operadores lógicos
+
+ 
---
+++ index.js
@@ -0,0 +1,15 @@
+const displayValorActual = document.getElementById("valorActual")
+const displayValorAnterior = document.getElementById("valorAnterior")
+
+const botonesNumeros = document.querySelectorAll(".numero")
+const botonesOperadores = document.querySelectorAll(".operador")
+
+const display = new Display(displayValorAnterior, displayValorActual)
+
+botonesNumeros.forEach( boton => {
+    boton.addEventListener("click", ()=> display.agregarNumero(boton.innerHTML))
+})
+
+botonesOperadores.forEach( boton => {
+    boton.addEventListener("click", ()=> display.computar(boton.value))
+})
